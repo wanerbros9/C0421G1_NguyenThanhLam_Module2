af724e351917435ba5a4a81c05a2f98d461b38d6
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-06-01 17:24:32 +0700

    initial commit

--- src/_01_introduction_java/bai_tap/Demo.java
+++
@@ -1,9 +0,0 @@
-package _01_introduction_java.bai_tap;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("one");
-        System.out.print("two");
-        System.out.print("three");
-    }
-}
---
+++ src/_01_introduction_java/thuc_hanh/SystemDate.java
@@ -0,0 +1,10 @@
+package _01_introduction_java.thuc_hanh;
+
+import java.util.Date;
+
+public class SystemDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: "+now);
+    }
+}
