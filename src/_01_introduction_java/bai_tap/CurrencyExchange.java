5a752896d1e905b57c7099c3f92d9c8cd2093283
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-06-02 23:28:54 +0700

    initial commit

--- src/_01_introduction_java/bai_tap/CurrencyExchange.java
+++ src/_01_introduction_java/bai_tap/CurrencyExchange.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class CurrencyExchange {
     public static void main(String[] args) {
-        int rate=23000;
+        int rate = 23000;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your money in USD: ");
         int CurrencyUSD = scanner.nextInt();
-        int CurrencyVND = rate*CurrencyUSD;
-        System.out.println("Your money in VND is: "+CurrencyVND+" VND");
+        int CurrencyVND = rate * CurrencyUSD;
+        System.out.println("Your money in VND is: " + CurrencyVND + " VND");
     }
 }
