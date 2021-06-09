031461cbae3a2f835281f4717a4c7cb29671fc06
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-06-09 16:48:59 +0700

    initial commit

--- src/_06_java_inheritance/bai_tap/PointClass_MovablePoint/MovablePoint.java
+++ src/_06_java_inheritance/bai_tap/PointClass_MovablePoint/MovablePoint.java
@@ -1,4 +1,60 @@
 package _06_java_inheritance.bai_tap.PointClass_MovablePoint;
 
-public class MovablePoint {
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed(){
+        float[] array = {this.xSpeed,getYSpeed()};
+        return array;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+
+    public Point move(){
+        this.setX(this.getX()+this.getXSpeed());
+        this.setY(this.getY()+this.getYSpeed());
+        return this;
+    }
 }
--- src/_06_java_inheritance/bai_tap/PointClass_MovablePoint/PointClass.java
+++
@@ -1,4 +0,0 @@
-package _06_java_inheritance.bai_tap.PointClass_MovablePoint;
-
-public class PointClass {
-}
