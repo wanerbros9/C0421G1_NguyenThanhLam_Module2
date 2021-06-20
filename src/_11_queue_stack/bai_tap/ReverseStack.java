e5f703b29654079fdc460df5013688957c939da0
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-06-21 00:02:46 +0700

    initial commit

--- src/_11_queue_stack/bai_tap/ReverseStack.java
+++ src/_11_queue_stack/bai_tap/ReverseStack.java
@@ -1,44 +1,54 @@
 package _11_queue_stack.bai_tap;
 
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class ReverseStack {
     public static void IntegerStack() {
-        Scanner n = new Scanner(System.in);
-        System.out.println("Enter array elements : ");
-        n = Scanner.next
-        int[] arr = new int[];
-
-        Stack<Integer> intStack = new Stack<>();
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Element you want to push: ");
-        int num = scanner.nextInt();
-        for (int i = 0; i < num; i++) {
-            System.out.print("Your number at " + i + " : ");
-            int number = scanner.nextInt();
-            intStack.push(number);
+        System.out.print("Enter element array: ");
+        int number = scanner.nextInt();
+        int[] arr = new int[number];
+        for (int i = 0; i < number; i++) {
+            System.out.print("Element at " + i + " : ");
+            arr[i] = scanner.nextInt();
+            stack1.push(arr[i]);
         }
-        System.out.println(intStack);
+//        System.out.println("Your Array: " + Arrays.toString(arr));
+        System.out.println("Stack: " + stack1);
 
-        for (int i = 0; i > 0; i--) {
-            int numPop = intStack.pop();
-            intStack.push(numPop);
+        for (int i = number; i > 0; i--) {
+            int numberPop = stack1.pop();
+            stack2.push(numberPop);
         }
-        System.out.println(intStack);
+        System.out.println("Reversed Stack: " + stack2);
     }
 
     private static void StringStack() {
-        Stack<String> wStack = new Stack<>();
         Scanner scanner = new Scanner(System.in);
+        Stack<String> wStack = new Stack<>();
         System.out.print("Enter your word: ");
         String inputWord = scanner.nextLine();
+        String[] mWord = inputWord.split(",", inputWord.length());
+        for (String string : mWord) {
+            wStack.push(string);
+        }
+        String outPut = "";
+        for (int i = mWord.length-1; i > 0; i--){
+            mWord[i] = wStack.pop();
+
+        }
+        outPut += mWord;
+        System.out.println(outPut.toString());
     }
 
     public static void main(String[] args) {
-        IntegerStack();
+//        IntegerStack();
         StringStack();
     }
 }
---
+++ src/_11_queue_stack/bai_tap/Test.java
@@ -0,0 +1,29 @@
+package _11_queue_stack.bai_tap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Test {
+//    public static void main(String[] args) {
+//        Stack<Integer> stack1 = new Stack<>();
+//        Stack<Integer> stack2 = new Stack<>();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("So phan tu cua mang: ");
+//        int number = scanner.nextInt();
+//        int[] arr = new int[number];
+//        for (int i = 0; i < number; i++) {
+//            System.out.print("phan tu tai " + i + " ");
+//            arr[i] = scanner.nextInt();
+//            stack1.push(arr[i]);
+//        }
+//        System.out.println("Mang cua ban: " + Arrays.toString(arr));
+//        System.out.println("Stack chua dao nguoc: " + stack1);
+//
+//        for (int i = number; i > 0; i--) {
+//            int numberPop = stack1.pop();
+//            stack2.push(numberPop);
+//        }
+//        System.out.println("Stack luc sau: " + stack2);
+//    }
+}
