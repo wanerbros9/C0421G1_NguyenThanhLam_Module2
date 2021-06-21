972675bef8f6ff1c5ed74bcc5e32835364168ae0
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-06-22 00:41:32 +0700

    initial commit

--- src/_11_queue_stack/bai_tap/TreeMap.java
+++ src/_11_queue_stack/bai_tap/TreeMap.java
@@ -11,5 +11,18 @@ public class TreeMap {
         String list = "Nguyen Thanh Lam";
         String[] array = list.toLowerCase().split("");
         System.out.println(Arrays.toString(array));
+        int count = 0;
+        for (String element : array) {
+            if (element.equals(",") || element.equals(" ")) {
+                continue;
+            }
+            if (myTreeMap.containsKey(element)) {
+                count = myTreeMap.get(element) + 1;
+                myTreeMap.put(element, count);
+            } else {
+                myTreeMap.put(element, 1);
+            }
+        }
+        System.out.println(myTreeMap);
     }
 }
