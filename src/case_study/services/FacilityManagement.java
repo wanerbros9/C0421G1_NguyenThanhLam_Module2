2d617c45685bbf35d01f418abaffd370d525ac0a
Nguyễn Thanh Lâm <wanerbros9@gmail.com>
2021-06-30 17:20:28 +0700

    initial commit

--- src/case_study/services/FacilityManagement.java
+++ src/case_study/services/FacilityManagement.java
@@ -1,6 +1,7 @@
 package case_study.services;
 
 import case_study.controllers.FuramaController;
+import case_study.models.Facility;
 
 import java.util.Scanner;
 
@@ -14,11 +15,12 @@ public class FacilityManagement {
         while (true) {
             System.out.println("1. Display list facility");
             System.out.println("2. Add new facility");
-            System.out.println("3. Display list facility");
+            System.out.println("3. Display list facility maintenance");
             System.out.println("4. Return main menu");
             int choice = input().nextInt();
             switch (choice) {
                 case 1:
+                    new FacilityServiceImpl().display();
                     break;
                 case 2:
                     break;
--- src/case_study/services/FacilityServiceImpl.java
+++ src/case_study/services/FacilityServiceImpl.java
@@ -1,6 +1,27 @@
 package case_study.services;
 
-public class FacilityServiceImpl implements FacilityService{
+import case_study.models.Facility;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FacilityServiceImpl implements FacilityService {
+    public static Scanner input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+
+    public static Map<String, Integer> facilities = new LinkedHashMap<>();
+
+    static {
+        facilities.put("Room", 0);
+        facilities.put("House", 0);
+        facilities.put("Villa", 0);
+    }
+
+
     @Override
     public void add() {
 
@@ -18,6 +39,10 @@ public class FacilityServiceImpl implements FacilityService{
 
     @Override
     public void display() {
-
+        for (Map.Entry<String, Integer> facility : facilities.entrySet()) {
+            if (facility != null) {
+                System.out.println(facility);
+            }
+        }
     }
 }
