f846aa0df5a14e5cc3b393a1b2de4d9554fe30b1
Nguyễn Thanh Lâm <49389939+wanerbros9@users.noreply.github.com>
2021-07-01 16:28:56 +0700

    initial commit

--- src/_18_java_string_regex/bai_tap/validate_class/ValidateClass.java
+++ src/_18_java_string_regex/bai_tap/validate_class/ValidateClass.java
@@ -7,7 +7,7 @@ public class ValidateClass {
     private static Pattern pattern;
     private Matcher matcher;
 
-    private static final String CLASS_REGEX = "^[CAP]{1}[0-9]{4}[GHIKLM]{1}[0-9]{1}";
+    private static final String CLASS_REGEX = "^[CAP][0-9]{4}[GHIKLM][0-9]$";
 
     public ValidateClass() {
     }
